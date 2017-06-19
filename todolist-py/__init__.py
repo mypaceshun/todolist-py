d0a94a6466a464ec73d9518f5d13461d229588e7
shun <shun@osstech.co.jp>
2017-06-19 17:03:30 +0900

    mkdir and touch __init__.py
    __init__.pyをとりあえず作ってみた
    シェバンなどはこれを参考に

---
+++ todolist-py/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# vi: set expandtab shiftwidth=4 softtabstop=4 : 
+#
+# Auther    shun
+# Date      2017/06/19
+# Filename  __init__.py
+#
+
+import os
+
